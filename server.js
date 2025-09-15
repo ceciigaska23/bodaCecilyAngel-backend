eb017da5a3832c475d14e6247e6ff84e85e2c8df
Lorena Cecilia Gasca Cortes <lgc0081027@officemax.net>
2025-09-15 12:50:49 -0600

    modificacion por no se cuanta vez

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // ===== URL DE GOOGLE APPS SCRIPT - ACTUALÍZALA =====
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxrsD50CB1stYTVnm_jcAmTXXXcHCghUdJ95aA6VnPSkeOwTRuBkRRW7STVGDGIWpn6/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz8ZRNpIcsMSSYGP2OB8gzFIzgBC5QTdCfAP0chfMcAVRbXT3fnilwXZL8JyQMm5T32/exec';
 
 // ===== RUTA DE SALUD PARA VERIFICAR QUE FUNCIONA =====
 app.get('/api/health', (req, res) => {
