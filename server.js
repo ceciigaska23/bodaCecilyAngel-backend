afe67a96aab4bdbf4c9c648b0768bd9f9192c949
Lorena Cecilia Gasca Cortes <lgc0081027@officemax.net>
2025-09-15 11:48:49 -0600

    se cambia orden de las funciones v2

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // ===== URL DE GOOGLE APPS SCRIPT - ACTUALÍZALA =====
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwX7LlgS8dzT0uMArC22rPGO3C1NPo9TT4RUtdLQeioP-iOPdNgfHutD5G-QLQDCrtQ/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzcfSdu4yBuq8AjmBVw4mKOGTgREkxQKwD7HlaBXKnuODyoTx2fUyplFCmhZDG-VmAx/exec';
 
 // ===== RUTA DE SALUD PARA VERIFICAR QUE FUNCIONA =====
 app.get('/api/health', (req, res) => {
