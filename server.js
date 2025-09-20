64a460ac13e557c5a1ada7e020e50baa908c8bc9
Lorena Cecilia Gasca Cortes <lgc0081027@officemax.net>
2025-09-20 17:48:50 -0600

    se cambia url api

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 
 // ===== URL DE GOOGLE APPS SCRIPT - ACTUALÍZALA =====
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyfTtYH72Yqxnw_v9UMnBWzz3i6rpRqdb4lZ7UflOcuei3VqrGnWTg794sA2NE6fZwc/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwl9wkGMWRzh5oy35U5bJP18SRvbqEOOSefAitT3tKsLT-Q6nxi614pslrmqK6OIyG8/exec';
 
 // ===== RUTAS DE LA API =====
 // Ruta de validación de código QR (nueva y corregida)
